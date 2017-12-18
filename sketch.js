883ac87e7f978ce8c75c30e7cfde0adb1991f8ee
helenemartin <helenemartin13@hotmail.com>
2017-12-18 20:42:53 +0000

    add blend mode DIFFERENCE on image.graphics instance

--- sketch.js
+++ sketch.js
@@ -5,21 +5,24 @@ var height;
 var width;
 var lines = [];
 var backgroundY = 0;
-var layer;
+var foregroundVideo;
+var backgroundVideo;
 var frame;
 
 function setup() {
-  canvas = createCanvas(1200, 1000);
-  layer = createVideo(['assets/5.mp4']);
-  layer.hide(); // by default video shows up in separate dom
-                  // element. hide it and draw it to the canvas
-                  // instead
-  layer.loop();
+  createCanvas(1200, 1000);
+  backgroundVideo = createVideo(['./assets/11.mp4']);
+  foregroundVideo = createVideo(['./assets/5.mp4']);
+  foregroundVideo.hide(); 
+  backgroundVideo.hide();
+  foregroundVideo.loop();
+  backgroundVideo.loop();
   graphic = createGraphics(1200, 1000);
   height = displayHeight;
   width = displayWidth;
-  graphic.stroke('rgba(0,255,0, 1)');
-  frame = createGraphics(400,200);
+  graphic.stroke('rgba(255,255,255, 0.3)');
+  frame = createGraphics(800,600);
+
 }
 function drawLine(lineData) {
   graphic.line(lineData.x,lineData.y,lineData.width,lineData.height);
@@ -28,13 +31,14 @@ function drawLine(lineData) {
 // create new graphic instance, draw a video to graphic, call image on that graphic,
 
 function draw() {
-  clear();
-  fill('#000');
+  // clear();
+  // fill(0, 102, 153);
   noStroke();
-  rect(0,backgroundY, width, height);
-  frame.image(video);
-  frame.blend;
-  image(frame);
+
+  // rect(0,backgroundY, width, height);
+  // frame.image(Video);
+  // frame.blend;
+  // image(frame);
   
 
   var lineData;
@@ -63,10 +67,13 @@ function draw() {
     // line(currentLine.x,currentLine.y,currentLine.width,currentLine.height);
 
   }
-  
-  layer.blend(graphic, 0, 0, width, height, 0, 0, width, height, DARKEST);
-  image(layer, 0, 0);
-  // image(graphic, 0, 0);
+  frame.image(foregroundVideo, 0, 0);
+  // frame.mask(graphic);
+  graphic.blend(frame.get(), 0, 0, width, height, 0, 0, width, height, DIFFERENCE);
+  // image(frame, 0, 0);
+  image(foregroundVideo,0,0);
+  image(backgroundVideo, 10, 10);
+  image(graphic, 0, 0);
 
   x = x + spacing;
   if (x > width) {
